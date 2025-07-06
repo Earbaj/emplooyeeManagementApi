3c0182b4946158e68c4a8d0630fbd161a6443981
Earbaj <earbajsaria3@gmail.com>
2025-07-06 13:58:30 +0600

    we create env and auth middle ware and jsone web tocken to secure api

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
 const db = require('../db');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 exports.register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -10,12 +12,33 @@ exports.register = async (req, res) => {
   });
 };
 
+// exports.login = (req, res) => {
+//   const { email, password } = req.body;
+//   db.query('SELECT * FROM users WHERE email = ?', [email], async (err, results) => {
+//     if (err || results.length === 0) return res.status(400).json({ error: 'Invalid credentials' });
+//     const match = await bcrypt.compare(password, results[0].password);
+//     if (!match) return res.status(401).json({ error: 'Invalid credentials' });
+//     res.json({ message: 'Login successful', user: results[0] });
+//   });
+// };
+
 exports.login = (req, res) => {
   const { email, password } = req.body;
   db.query('SELECT * FROM users WHERE email = ?', [email], async (err, results) => {
     if (err || results.length === 0) return res.status(400).json({ error: 'Invalid credentials' });
     const match = await bcrypt.compare(password, results[0].password);
     if (!match) return res.status(401).json({ error: 'Invalid credentials' });
-    res.json({ message: 'Login successful', user: results[0] });
+
+    const token = jwt.sign({ id: results[0].id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    res.json({
+      message: 'Login successful',
+      token,
+      user: {
+        id: results[0].id,
+        name: results[0].name,
+        email: results[0].email
+      }
+    });
   });
 };
--- routes/employeeRoutes.js
+++ routes/employeeRoutes.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const emp = require('../controllers/employeeController');
 
-router.get('/api/employees', emp.getAll);
-router.get('/api/employees/:id', emp.getOne);
-router.post('/api/employees', emp.create);
-router.put('/api/employees/:id', emp.update);
-router.delete('/api/employees/:id', emp.delete);
+const auth = require('../middleware/authMiddleware');
+
+router.get('/api/employees', auth,emp.getAll);
+router.get('/api/employees/:id',  auth,emp.getOne);
+router.post('/api/employees',  auth,emp.create);
+router.put('/api/employees/:id',  auth,emp.update);
+router.delete('/api/employees/:id',  auth,emp.delete);
 
 module.exports = router;
