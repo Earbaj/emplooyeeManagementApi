58cf4e733278f3c7fabd84ca269591b5713f9eb6
Earbaj <earbajsaria3@gmail.com>
2025-07-06 23:28:52 +0600

    updated db.js

--- db.js
+++ db.js
@@ -22,16 +22,25 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect(err => {
-  if (err) throw err;
-  console.log('MySQL Connected');
+// Optional: test the connection and log
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('[DB ERROR]', err);
+  } else {
+    console.log('✅ MySQL DB Connected');
+    connection.release();
+  }
 });
 
 module.exports = db;
